4b2db5a589df57740575855b8d5e6f7aec81b029
jungdadaeng <jungdayoonyi@gmail.com>
2023-03-16 22:30:25 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from 'react';
+// import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const App = () => {
+//   const [data, setData] = useState(null);
+//   const onClick = async () => {
+//     try{
+//       const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=494b5446f57c4ceea44d6b2f5778ea15');
+//       setData(response.data);
+//     }catch(e){
+//       console.log(e);
+//     }
+    
+//   }
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+//       {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
+//     </div>
+//   )
+// }
+
+import NewsList from './components/NewsList';
+
+const App = () => {
+  return <NewsList/>
 }
 
 export default App;
